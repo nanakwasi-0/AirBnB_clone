408fe933fdeef39d086616dfdf6fd76fd3ab8c67
nanakwasi-0 <nankwasi999@gmail.com>
2024-01-16 15:15:57 -0800

    Place refactoring

--- models/place.py
+++ models/place.py
@@ -24,10 +24,10 @@ class Place(BaseModel):
     user_id = ""
     name = ""
     description = ""
-    max_guest = 0
     number_rooms = 0
     number_bathrooms = 0
+    max_guest = 0
     price_by_night = 0
-    amenity_ids = []
     latitude = 0.0
     longitude = 0.0
+    amenity_ids = []
